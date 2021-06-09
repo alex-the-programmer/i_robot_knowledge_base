2290f89fa38f579f64e497dabe5ff1891f6b8097
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2021-06-08 21:08:53 -0700

    seed data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Concept.create!([
+  {name: "noun", concept_type_id: nil, ml_model_id: nil},
+  {name: "object", concept_type_id: nil, ml_model_id: nil},
+  {name: "quality", concept_type_id: nil, ml_model_id: nil},
+  {name: "dimension", concept_type_id: nil, ml_model_id: nil},
+  {name: "weight", concept_type_id: nil, ml_model_id: nil},
+  {name: "height", concept_type_id: nil, ml_model_id: nil},
+  {name: "length", concept_type_id: nil, ml_model_id: nil},
+  {name: "radius", concept_type_id: nil, ml_model_id: nil},
+  {name: "color", concept_type_id: nil, ml_model_id: nil},
+  {name: "material", concept_type_id: nil, ml_model_id: nil},
+  {name: "width", concept_type_id: nil, ml_model_id: nil},
+  {name: "table", concept_type_id: nil, ml_model_id: nil},
+  {name: "wood", concept_type_id: nil, ml_model_id: nil},
+  {name: "plate", concept_type_id: nil, ml_model_id: nil}
+])
+ConceptType.create!([
+  {name: "adjective"},
+  {name: "noun"}
+])
+ConceptRelationshipType.create!([
+  {name: "is_a"}
+])
+Operation.create!([
+  {name: "pick", concept_id: 2},
+  {name: "go_to", concept_id: 2},
+  {name: "put_at", concept_id: 2},
+  {name: "move", concept_id: 2}
+])
+ConceptRelationship.create!([
+  {parent_concept_id: 1, child_concept_id: 2, concept_relationship_type_id: 1},
+  {parent_concept_id: 1, child_concept_id: 3, concept_relationship_type_id: 1},
+  {parent_concept_id: 3, child_concept_id: 4, concept_relationship_type_id: 1},
+  {parent_concept_id: 4, child_concept_id: 7, concept_relationship_type_id: 1},
+  {parent_concept_id: 4, child_concept_id: 6, concept_relationship_type_id: 1},
+  {parent_concept_id: 4, child_concept_id: 8, concept_relationship_type_id: 1},
+  {parent_concept_id: 3, child_concept_id: 10, concept_relationship_type_id: 1},
+  {parent_concept_id: 3, child_concept_id: 11, concept_relationship_type_id: 1},
+  {parent_concept_id: 4, child_concept_id: 9, concept_relationship_type_id: 1},
+  {parent_concept_id: 3, child_concept_id: 5, concept_relationship_type_id: 1},
+  {parent_concept_id: 2, child_concept_id: 12, concept_relationship_type_id: 1},
+  {parent_concept_id: 11, child_concept_id: 13, concept_relationship_type_id: 1},
+  {parent_concept_id: 2, child_concept_id: 14, concept_relationship_type_id: 1}
+])
+Argument.create!([
+  {operation_id: 1, name: "subject", base_concept_id: 2, enum_values: nil},
+  {operation_id: 2, name: "destination", base_concept_id: 2, enum_values: nil},
+  {operation_id: 3, name: "destination", base_concept_id: 2, enum_values: nil},
+  {operation_id: 3, name: "position", base_concept_id: nil, enum_values: "on_top,inside"},
+  {operation_id: 4, name: "subject", base_concept_id: 2, enum_values: nil},
+  {operation_id: 4, name: "destination", base_concept_id: 2, enum_values: nil},
+  {operation_id: 4, name: "position", base_concept_id: nil, enum_values: "on_top,inside"}
+])
